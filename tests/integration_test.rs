018db8eba27af5e76021d9436013c54564a76144
hussainsultan <hussainz@gmail.com>
2023-12-21 06:53:11 -0500

    add test to train a model

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,7 +1,8 @@
 use datafusion::assert_batches_eq;
 use datafusion::error::Result;
 use datafusion::{execution::options::CsvReadOptions, prelude::SessionContext};
-use xgboost_udf_example::{create_dmatrix, register_udfs};
+use xgboost_udf_example::{create_dmatrix, register_udfs, convert_to_native};
+use xgboost::{parameters, Booster};
 
 #[tokio::test]
 async fn it_onehots() -> Result<()> {
@@ -41,6 +42,46 @@ async fn it_creates_dmatrix() -> Result<()> {
     let batches = ctx.sql(sql).await?.collect().await?;
     let dmat = create_dmatrix(&batches[0])?;
     assert_eq!(dmat.shape(), (8124, 22));
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn it_trains_a_model() -> Result<()> {
+    let ctx = SessionContext::new();
+    register_udfs(&ctx);
+    ctx.register_csv("mushrooms", "./data/mushrooms.csv", CsvReadOptions::new())
+        .await?;
+    // how do I make this string multiline?
+    let sql = "SELECT onehot(arrow_cast(cap_shape, 'Dictionary(Int32, Utf8)')) as cap_shap, onehot(arrow_cast(cap_surface, 'Dictionary(Int32, Utf8)')) as cap_surface, onehot(arrow_cast(cap_color, 'Dictionary(Int32, Utf8)')) as cap_color, onehot(arrow_cast(bruises, 'Dictionary(Int32, Utf8)')) as bruises FROM mushrooms";
+    let batches = ctx.sql(sql).await?.collect().await?;
+    let mut dmat = create_dmatrix(&batches[0])?;
+    let sql = "SELECT onehot(arrow_cast(class, 'Dictionary(Int32, Utf8)')) as class FROM mushrooms";
+    let classes = ctx.sql(sql).await?.collect().await?;
+    let (classes, _) = convert_to_native(&classes[0].column(0), 1)?;
+    let data_transform = classes
+        .into_iter()
+        .map(|x| x as u8 as f32)
+        .collect::<Vec<f32>>();
+
+    dmat.set_labels(&data_transform).unwrap();
+    let evaluation_sets = &[(&dmat, "train")];
+
+    // specify overall training setup
+    let training_params = parameters::TrainingParametersBuilder::default()
+        .dtrain(&dmat)
+        .evaluation_sets(Some(evaluation_sets))
+        .build()
+        .unwrap();
+
+    // train model, and print evaluation data
+    let bst = Booster::train(&training_params).unwrap();
+    
+    println!("{:?}", bst.predict(&dmat.slice(&[1,5]).unwrap()).unwrap());
+    bst.save("model.xgb").unwrap();
+
+
+
 
     Ok(())
 }
