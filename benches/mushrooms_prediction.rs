ced487bb290bcbcf6aba01af08f7e714d0e96629
hussainsultan <hussainz@gmail.com>
2023-12-24 05:11:11 -0500

    add setup for mushroom benchmark by moving the SessionContext and udf registration to setup

--- benches/mushrooms_prediction.rs
+++ benches/mushrooms_prediction.rs
@@ -6,19 +6,21 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 use tokio::runtime::Runtime;
 
-async fn score() -> Result<()> {
-    let ctx = SessionContext::new();
-    register_udfs(&ctx);
-    ctx.register_csv("mushrooms", "./data/mushrooms.csv", CsvReadOptions::new())
-        .await?;
-    let sql = "SELECT predict(cap_shape,cap_surface,cap_color,bruises) as predictions FROM (SELECT onehot(arrow_cast(cap_shape, 'Dictionary(Int32, Utf8)')) as cap_shape, onehot(arrow_cast(cap_surface, 'Dictionary(Int32, Utf8)')) as cap_surface, onehot(arrow_cast(cap_color, 'Dictionary(Int32, Utf8)')) as cap_color, onehot(arrow_cast(bruises, 'Dictionary(Int32, Utf8)')) as bruises FROM mushrooms) data";
+async fn score(ctx: &SessionContext, sql: &str) -> Result<()> {
+    ctx.register_csv("mushrooms", "./data/mushrooms.csv", CsvReadOptions::new()).await?;
     let _batches = ctx.sql(sql).await?.collect().await?;
     Ok(())
 }
 
 fn bench_mushrooms_prediction(c: &mut Criterion) {
-    c.bench_function("mushrooms_prediction", move |b| {
-        b.to_async(Runtime::new().unwrap()).iter(|| score())
+    let ctx = SessionContext::new();
+    register_udfs(&ctx);
+    let sql = "SELECT predict(cap_shape,cap_surface,cap_color,bruises) as predictions FROM (SELECT onehot(arrow_cast(cap_shape, 'Dictionary(Int32, Utf8)')) as cap_shape, onehot(arrow_cast(cap_surface, 'Dictionary(Int32, Utf8)')) as cap_surface, onehot(arrow_cast(cap_color, 'Dictionary(Int32, Utf8)')) as cap_color, onehot(arrow_cast(bruises, 'Dictionary(Int32, Utf8)')) as bruises FROM mushrooms) data";
+    
+
+    c.bench_function("mushrooms_prediction", |b| {
+        b.to_async(Runtime::new().unwrap()).iter( || 
+            score(&ctx, &sql))
     });
 }
 
