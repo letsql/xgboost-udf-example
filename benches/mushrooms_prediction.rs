220ffe9ea3924080e3e36482be861430e78191c5
hussainsultan <hussainz@gmail.com>
2023-12-30 12:23:35 -0500

    format

--- benches/mushrooms_prediction.rs
+++ benches/mushrooms_prediction.rs
@@ -16,11 +16,10 @@ fn bench_mushrooms_prediction(c: &mut Criterion) {
     let _ = ctx.register_csv("mushrooms", "./data/mushrooms.csv", CsvReadOptions::new());
     register_udfs(&ctx);
     let sql = "SELECT predict(cap_shape,cap_surface,cap_color,bruises) as predictions FROM (SELECT onehot(arrow_cast(cap_shape, 'Dictionary(Int32, Utf8)')) as cap_shape, onehot(arrow_cast(cap_surface, 'Dictionary(Int32, Utf8)')) as cap_surface, onehot(arrow_cast(cap_color, 'Dictionary(Int32, Utf8)')) as cap_color, onehot(arrow_cast(bruises, 'Dictionary(Int32, Utf8)')) as bruises FROM mushrooms) data";
-    
 
     c.bench_function("mushrooms_prediction", |b| {
-        b.to_async(Runtime::new().unwrap()).iter( || 
-            run_sql(&ctx, &sql))
+        b.to_async(Runtime::new().unwrap())
+            .iter(|| run_sql(&ctx, &sql))
     });
 }
 
@@ -29,11 +28,10 @@ fn bench_mushrooms_onehot(c: &mut Criterion) {
     let _ = ctx.register_csv("mushrooms", "./data/mushrooms.csv", CsvReadOptions::new());
     register_udfs(&ctx);
     let sql = "SELECT onehot(arrow_cast(cap_shape, 'Dictionary(Int32, Utf8)')) as cap_shape, onehot(arrow_cast(cap_surface, 'Dictionary(Int32, Utf8)')) as cap_surface, onehot(arrow_cast(cap_color, 'Dictionary(Int32, Utf8)')) as cap_color, onehot(arrow_cast(bruises, 'Dictionary(Int32, Utf8)')) as bruises FROM mushrooms";
-    
 
     c.bench_function("mushrooms_prediction", |b| {
-        b.to_async(Runtime::new().unwrap()).iter( || 
-            run_sql(&ctx, &sql))
+        b.to_async(Runtime::new().unwrap())
+            .iter(|| run_sql(&ctx, &sql))
     });
 }
 
